c8acd418a66b0f463d52fd71175a8e67b5df8d0a
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-09 22:29:25 +0300

    fix rendering that broke with r-s-m version 2 updgrade

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,32 +1,27 @@
 import React from 'react'
 import { ComposableMap, ZoomableGroup, Geographies, Geography, Markers } from "react-simple-maps"
 
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
+
+
 export default function MyMap({ zoom, markers, setZoom }) {
     return (
         <div className="map">
             <ComposableMap style={{ width: "100%", height: "auto" }}>
                 <ZoomableGroup zoom={zoom}>
-                    <Geographies geography={"https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"}>
-                        {(geographies, projection) => geographies.map((geography, i) => (
+                    <Geographies geography={geoUrl}>
+                        {({ geographies, projection }) => geographies.map((geography, i) => (
                             <Geography
                                 key={i}
                                 geography={geography}
                                 projection={projection}
                             />
-                        ))}
+                        ))
+                        }
                     </Geographies>
-                    <Markers>
-                        {markers}
-                    </Markers>
+                    {markers}
                 </ZoomableGroup>
             </ComposableMap>
-
-            <div className="zoom-controls">
-                <button onClick={() => setZoom(zoom * 2)}>Zoom in</button>
-                <button onClick={() => setZoom(zoom / 2)}>Zoom out</button>
-            </div>
-
-
         </div>
     )
 }
