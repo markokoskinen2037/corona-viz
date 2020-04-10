8aa9254ea5e2ebe38124c081f95dfc4cd45aa34b
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-10 15:29:33 +0300

    removed unused stuff

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { ComposableMap, ZoomableGroup, Geographies, Geography, Markers } from 'react-simple-maps'
+import { ComposableMap, ZoomableGroup, Geographies, Geography } from 'react-simple-maps'
 
 const geoUrl = 'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
 
-export default function MyMap({ zoom, markers, setZoom }) {
+export default function MyMap({ markers }) {
   return (
     <div className="map">
       <ComposableMap style={{ width: '100%', height: 'auto' }}>
-        <ZoomableGroup zoom={zoom}>
-          <Geographies geography={geoUrl}>
-            {({ geographies, projection }) => geographies.map((geography, i) => <Geography key={i} geography={geography} projection={projection} />)}
-          </Geographies>
+        <ZoomableGroup zoom={1.2}>
+          <Geographies geography={geoUrl}>{({ geographies }) => geographies.map((geography, i) => <Geography key={i} geography={geography} />)}</Geographies>
           {markers}
         </ZoomableGroup>
       </ComposableMap>
